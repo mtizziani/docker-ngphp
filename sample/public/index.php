17fc96837605e80f942d153c530334941d5fb04c
Maik Tizziani <mtizziani@gmail.com>
2024-02-19 03:09:31 +0100

    adding copyright and license information to files

--- sample/public/index.php
+++ sample/public/index.php
@@ -1,3 +1,9 @@
+<!--
+## License and Copyright
+### This file is port of the "ngphp" project, copyright 2024 by Maik Tizziani
+### The project is published under let LGPL3 License
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <body>
